c97c100c85ffeb2457a090e02f7848a16f3bebc8
DESKTOP-OSSCN77\yingy <yingyugang@gmail.com>
2021-05-06 13:38:42 +0900

    comment out obsolete

--- main.go
+++ main.go
@@ -1,25 +1,23 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"github.com/sideshow/apns2"
 	"github.com/sideshow/apns2/certificate"
 	"log"
-	"os"
 )
 /*
-	apnsでプッシューを送る、テストずみ。
+ * apnsでプッシューを送る、テストずみ。
  */
 func main() {
-	certPath := flag.String("cert", "../cert.p12", "")
-	token := flag.String("token", "11aa01229f15f0f0c52029d8cf8cd0aeaf2365fe4cebc4af26cd6d76b7919ef7", "")
-	topic := flag.String("topic", "com.moba1", "")
-	flag.Parse()
-	if *certPath == "" || *token == "" || *topic == "" {
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
+	//certPath := flag.String("cert", "../cert.p12", "")
+	//token := flag.String("token", "4648c0188ea303345012df2dee723b509dc2d1dab470cb0adb3515914156ef95", "")
+	//topic := flag.String("topic", "com.moba1", "")
+	//flag.Parse()
+	//if *certPath == "" || *token == "" || *topic == "" {
+	//	flag.PrintDefaults()
+	//	os.Exit(1)
+	//}
 	//用工程下的APNsDev.p12
 	cert, err := certificate.FromP12File("C:/Users/yingy/Documents/IOSRemotePushServer_APNS2/APNsDev.p12", "1fd94d19bbe3c")
 	if err != nil {
@@ -27,14 +25,17 @@ func main() {
 	}
 	notification := &apns2.Notification{}
 	//在XCodeLog里面找到DeviceToken在这里替换。每次重新安装应用的时候会产生新的Token。
-	notification.DeviceToken = "0dd11ed4d1219b2e1f7a65f56a0e22bbb40a2f292d72eba4a2dd317c5f639950"
+	notification.DeviceToken = "a467c01f57345848a5167e8d0f55b871064dcb5f73b81c82734c71ec18340b1b"
 	notification.Topic = "com.moba"
 	notification.Payload = []byte(`{
 			"aps" : {
-				"alert" : "Hello!"
+				"alert":"Hello!",
+        		"badge":1,
+        		"mutable-content":1
 			},
 			"image-url": "https://yingyugang.s3-ap-northeast-1.amazonaws.com/goldIco.png"
 		}`)
+
 	client := apns2.NewClient(cert).Development()//   Production()
 	res, err := client.Push(notification)
 	if err != nil {
