cabbc4b842ab968862a6f70d2c736fdc2ca2e717
DESKTOP-OSSCN77\yingy <yingyugang@gmail.com>
2021-05-12 11:45:28 +0900

    Support http.

--- main.go
+++ main.go
@@ -5,11 +5,25 @@ import (
 	"github.com/sideshow/apns2"
 	"github.com/sideshow/apns2/certificate"
 	"log"
+	"net/http"
 )
+
 /*
  * apnsでプッシューを送る、テストずみ。
  */
 func main() {
+	http.HandleFunc("/getDeviceToken", getDeviceToken)
+	http.HandleFunc("/sendSampleAPNs", sendSampleAPNs)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+var deviceToken string
+
+func getDeviceToken(w http.ResponseWriter, r *http.Request){
+	deviceToken = r.URL.Query()["deviceToken"][0]
+}
+
+func sendSampleAPNs(w http.ResponseWriter, r *http.Request){
 	//用工程下的APNsDev.p12
 	cert, err := certificate.FromP12File("C:/Users/yingy/Documents/IOSRemotePushServer_APNS2/APNsDev.p12", "1fd94d19bbe3c")
 	if err != nil {
@@ -17,7 +31,10 @@ func main() {
 	}
 	notification := &apns2.Notification{}
 	//在XCodeLog里面找到DeviceToken在这里替换。每次重新安装应用的时候会产生新的Token。
-	notification.DeviceToken = "79ecd5681860feadc8579353b1fa4d98f79de7defa9bed371d6e37571ba4ba80"
+	notification.DeviceToken = "5b4e466af6171c9991e04f8934ed7247702c02e70c3dd59dc741b36ec006189d"
+	if len(deviceToken) > 0 {
+		notification.DeviceToken = deviceToken
+	}
 	notification.Topic = "com.moba"
 	notification.Payload = []byte(`{
 			"aps" : {
